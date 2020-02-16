7351c60f3d9846f1613231e5963777234dffe3c3
kimihito <tatsurotamashiro@gmail.com>
2020-02-17 06:39:10 +0900

    JSONファイルとして出力する

--- apps/crawler/src/index.ts
+++ apps/crawler/src/index.ts
@@ -1,11 +1,14 @@
 import { EpisodeCrawler } from './episodeCrawler'
 import { EpisodePathsCrawler } from './episodePathsCrawler'
 import { Episode } from './episode'
-import * as path from 'path'
 import puppeteer from 'puppeteer'
-import { pathToFileURL  } from 'url'
+import lowdb from 'lowdb'
+import FileAsync from 'lowdb/adapters/FileAsync'
 import { RestaurantsCrawler } from './restaurantsCrawler'
 
+
+const adapter = new FileAsync<Episode[]>('./db.json')
+const db = lowdb(adapter)
 const launchArgs: puppeteer.ChromeArgOptions = {
   args: [
     '--disable-setuid-sandbox',
@@ -29,27 +32,31 @@ const currentIndexPage = (index: number): string => {
 
 const main = async () => {
   try {
+    (await db).defaults({ episodes: [] }).write()
     let index = 0
     const browser = await puppeteer.launch(launchArgs)
     const page = await browser.newPage()
 
     while (true) {
+      if (index === 20) break // debug
       console.log(currentIndexPage(index))
       const paths = await new EpisodePathsCrawler(page, currentIndexPage(index)).run()
+      if (!paths.length) break
       index += 20
       for (let path of paths) {
+        console.log(`goto ${currentPage(path)}...`)
         await page.goto(currentPage(path))
         const episode = await new EpisodeCrawler(page).run()
         if (episode) {
           episode.restaurants = await new RestaurantsCrawler(page).run()
           episodes.push(episode)
+          const data: any = (await db).get('episodes')
+          await data.push(episode).write()
         }
       }
-      if ((isDevelopment && index === 20)) break //debug
     }
 
     console.log(episodes)
-
     await browser.close()
 
     console.log('fin')
