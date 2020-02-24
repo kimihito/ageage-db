cffb04c210aab5e117e219eea89d6398552d9c8c
kimihito <tatsurotamashiro@gmail.com>
2020-02-24 17:16:48 +0900

    material-tableで表示する

--- apps/web/src/restaurantList.tsx
+++ apps/web/src/restaurantList.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { RestaurantWithEpisode } from './entities'
-import { Container, Typography, Box, Link, Table, TableBody, Grid, TableContainer, TableHead, TableRow, TableCell } from '@material-ui/core'
+import MaterialTable from 'material-table'
+import { Container, Typography, Box, Link,  Grid } from '@material-ui/core'
 
 interface Props {
     restaurants: RestaurantWithEpisode[]
+}
+
+const tableTitle = (): JSX.Element => {
+    return (
+    <Typography>
+        <Link href="https://www.otv.co.jp/ageage/" target="_blank" rel="noopener">
+            アゲアゲ飯
+        </Link>
+        で紹介されたお店をテーマごとに検索します
+    </Typography>
+
+    )
 }
 
 const RestaurantList = (props: Props) => {
@@ -11,56 +24,26 @@ const RestaurantList = (props: Props) => {
         <Box m={1}>
             <Container maxWidth="lg">
                 <Grid xs={12}>
-                    <Typography>
-                        <Link href="https://www.otv.co.jp/ageage/" target="_blank" rel="noopener">
-                            アゲアゲ飯
-                        </Link>
-                        で紹介されたお店をテーマごとに検索します
-                    </Typography>
-                    <TableContainer >
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>
-                                        店名
-                                    </TableCell>
-                                    <TableCell>
-                                        住所
-                                    </TableCell>
-                                    <TableCell>
-                                        営業時間
-                                    </TableCell>
-                                    <TableCell>
-                                        定休日
-                                    </TableCell>
-                                    <TableCell>
-                                        エピソード名
-                                    </TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {props.restaurants.map(r => (
-                                    <TableRow key={r.id}>
-                                        <TableCell component="th" scope="row">
-                                            {r.name}
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            {r.address}
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            {r.open}
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            {r.close}
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            {r.episode.theme}
-                                        </TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
+                    <MaterialTable
+                        columns={[
+                            {title: 'テーマ', field: 'episode.theme'},
+                            {title: '店名', field: 'name'},
+                            {title: '住所', field: 'address'},
+                            {title: '営業時間', field: 'open'},
+                            {title: '定休日', field: 'close'},
+                        ]}
+                        data={props.restaurants}
+                        title={
+                            <Typography>
+                            <Link href="https://www.otv.co.jp/ageage/" target="_blank" rel="noopener">
+                                アゲアゲ飯
+                            </Link>
+                            で紹介されたお店をテーマごとに検索します
+                            </Typography>
+                        }
+                    >
+
+                    </MaterialTable>
                 </Grid>
             </Container>
         </Box>
