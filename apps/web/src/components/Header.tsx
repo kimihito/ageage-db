8d0693a1c389d848fed7543e4aca3bea49e16de3
kimihito <tatsurotamashiro@gmail.com>
2020-02-24 18:37:09 +0900

    HeaderをSFCで書き換える

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,19 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { AppBar, Container, Typography, Toolbar } from '@material-ui/core'
 
-class Header extends Component {
-    render() {
-        return(
-            <AppBar position="static">
-                <Container maxWidth="lg">
-                    <Toolbar>
-                        <Typography variant="h6">Ageage-db</Typography>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-        )
-    }
-
+const Header = () => {
+    return (
+        <AppBar position="static">
+            <Container maxWidth="lg">
+                <Toolbar>
+                    <Typography variant="h6">Ageage-db</Typography>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
 }
 
 export default Header
